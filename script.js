09078dbb722ef9f4d94e577e3648b4e1b75d9e4d
Abdoulaye Diallo <elhadj.24dia@gmail.com>
2025-05-04 08:43:34 -0400

    prémière version du projet

---
+++ script.js
@@ -0,0 +1,39 @@
+//Fonction pour la recherche:
+
+function searchKeyword() {
+    const input = document.getElementById("searchInput").value.trim().toLowerCase();
+    const elements = document.querySelectorAll("p, h1, h2, h3");
+
+    if (input === "") {
+        alert("Entrez un mot à chercher.");
+        return;
+    }
+
+    let found = false;
+    let firstMatchScrolled = false;
+
+    elements.forEach(element => {
+        element.innerHTML = element.textContent; // Reset le contenu pour enlever les anciens <mark>
+        const text = element.textContent;
+        const lowerText = text.toLowerCase();
+
+        if (lowerText.includes(input)) {
+            found = true;
+
+            // Surligner le texte
+            const regex = new RegExp(`(${input})`, "gi");
+            element.innerHTML = text.replace(regex, '<mark>$1</mark>'); 
+
+            // Scroll vers le premier match seulement
+            if (!firstMatchScrolled) {
+                element.scrollIntoView({ behavior: "smooth", block: "center" });
+                firstMatchScrolled = true;
+            }
+            setTimeout(() => { element.innerHTML = element.textContent;} , 5000 )
+        }
+    });
+
+    if (!found) {
+        alert("Aucun résultat trouvé.");
+    }
+}
